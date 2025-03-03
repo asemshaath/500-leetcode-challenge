4a539de39a51370b1d00091ebc93a4b0738db70f
Asem Shaath <shaathasem@gmail.com>
2025-03-03 00:06:36 -0500

    updated LC 11

--- leetcode/container-most-water-11/main.py
+++ leetcode/container-most-water-11/main.py
@@ -12,3 +12,21 @@
                 r -= 1
         return water
 
+
+# new sol march 3rd, 2025
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        water = 0
+
+        while r - l > 0:
+            water = max(water, (r-l)*min(height[l], height[r]))
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
+        return water
+
+
+
+
