13df8e51792b57977239ab9d9cbe15f62d938db4
Asem Shaath <shaathasem@gmail.com>
2025-04-03 00:25:10 -0400

    updated LC 153

--- leetcode/find-min-rotated-sorted-arr-153/main.py
+++ leetcode/find-min-rotated-sorted-arr-153/main.py
@@ -14,3 +14,34 @@ class Solution:
         return min(nums[l], nums[r])
 
             
+
+
+# update to better solution
+
+class Solution2:
+    def findMin(self, nums: List[int]) -> int:
+        # 4,5,6,0,1,2
+        # l     m   r
+        # 
+        # l < r return l
+
+        l = 0 
+        r = len(nums) - 1
+        mini = 5000000
+
+        while l <= r:
+            if nums[l] <= nums[r]:
+                mini = min(mini, nums[l])
+                return mini
+            
+            m = (r + l) // 2
+
+            if nums[m] >= nums[l]:
+                mini = min(mini, nums[m])
+                l = m + 1
+            else:
+                mini = min(mini, nums[m])
+                r = m - 1
+        return nums[l]
+
+
