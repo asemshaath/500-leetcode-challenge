07497e3887b1a4028875824681270b0dbbcd07d6
Asem Shaath <shaathasem@gmail.com>
2025-08-23 19:58:49 -0400

    updated LC 200

--- leetcode/num-islands-200/main.py
+++ leetcode/num-islands-200/main.py
@@ -36,3 +36,50 @@
                     num_islands += 1
 
         return num_islands
+
+
+
+
+# new solution for recursive approach
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        num_islands = 0
+        visited = set()
+
+        def dfs(x, y):
+            
+            if (not isValidIndex(x, y) or
+                grid[x][y] != "1" or
+                (x, y) in visited):
+                return 
+
+            visited.add((x, y))
+            dfs(x+1, y)
+            dfs(x-1, y)
+            dfs(x, y+1)
+            dfs(x, y-1)
+            
+
+        
+        def isValidIndex(i, j):
+            if i < 0 or j < 0:
+                return False
+            elif i >= len(grid): 
+                return False
+            elif j >= len(grid[0]):
+                return False
+            return True
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                curr = grid[i][j]
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    dfs(i, j)
+                    num_islands += 1
+
+        return num_islands
+
+
+
