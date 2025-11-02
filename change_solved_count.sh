afb5f06ad5164df883b0cc8ca1306e0442a3624e
Asem Shaath <shaathasem@gmail.com>
2025-11-01 22:33:46 -0400

    added commit functionality

--- change_solved_count.sh
+++ change_solved_count.sh
@@ -4,3 +4,7 @@ NUM_LINES=$(ls -l $1 | wc -l)
 echo "Number of solved leetcode problems: $NUM_LINES"
 
 sed -i "s/Problems solved:.*/Problems solved: $NUM_LINES/" README.md
+
+git add README.md
+
+git commit -m "readme: solved count modification"
