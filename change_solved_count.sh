4d7892206b9288c325efaba7abfd67afca51a4c4
Asem Shaath <60518004+asemshaath@users.noreply.github.com>
2025-11-02 22:13:17 -0500

    change_solved_count.sh: eliminate git commands
    Removed git commands to streamline the script.

--- change_solved_count.sh
+++ change_solved_count.sh
@@ -5,6 +5,3 @@ echo "Number of solved leetcode problems: $NUM_LINES"
 
 sed -i "s/Problems solved:.*/Problems solved: $NUM_LINES/" README.md
 
-git add README.md
-
-git commit -m "readme: solved count modification"
