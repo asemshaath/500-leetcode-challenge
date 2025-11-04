5ca55f18b381b236889c707aafa32ac3e29e9098
Asem Shaath <shaathasem@gmail.com>
2025-11-03 20:14:55 -0500

    resolved LC 355

--- leetcode/design-twitter-355/main.py
+++ leetcode/design-twitter-355/main.py
@@ -42,3 +42,82 @@ class Twitter:
 # param_2 = obj.getNewsFeed(userId)
 # obj.follow(followerId,followeeId)
 # obj.unfollow(followerId,followeeId)
+
+
+
+# New solutions to use heaps.
+# The previous solution did not use the heap.
+
+class Twitter2:
+
+    """
+    postTweet will append the tweet into tweets list
+    follow will add 
+    """
+    def __init__(self):
+        self.followings = dict()
+        self.tweets_per_user = dict()
+        self.time = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        
+        if userId not in self.tweets_per_user:
+            self.tweets_per_user[userId] = [(self.time, tweetId)]
+        else:
+            self.tweets_per_user[userId].append((self.time, tweetId))
+        
+        self.time -= 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        feed = []
+        heap = []
+        
+        # if userId not in self.tweets_per_user:
+        #     self.tweets_per_user[userId] = []
+
+        if userId in self.followings:
+            self.followings[userId].add(userId)
+        else:
+            self.followings[userId] = set({userId})
+
+        for followee_id in self.followings.get(userId, {}):
+            if followee_id in self.tweets_per_user:
+                # heap.extend(self.tweets_per_user[followee_id])
+                index = len(self.tweets_per_user[followee_id]) - 1
+                time, tweet = self.tweets_per_user[followee_id][index]
+                heap.append((time, tweet, followee_id, index - 1))
+
+        heapq.heapify(heap)
+
+        while heap and len(feed) < 10:
+            _, tweet, followee_id, index = heapq.heappop(heap)
+            feed.append(tweet)
+
+            if index >= 0:
+                time, new_tweet = self.tweets_per_user[followee_id][index]
+                heapq.heappush(heap, (time, new_tweet, followee_id, index - 1))
+
+        return feed
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followeeId == followerId:
+            return
+
+        if self.followings.get(followerId):
+            self.followings[followerId].add(followeeId)
+        else:
+            self.followings[followerId] = set({followeeId})
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId == followerId:
+            return
+
+        if self.followings.get(followerId):
+            self.followings[followerId].remove(followeeId)
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
